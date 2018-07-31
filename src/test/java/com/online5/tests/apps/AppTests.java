88e8f259aa9802e975666dbcaa31125d6174cf80
mehmetucar86@gmail.com <mehmetucar86@gmail.com>
2018-07-31 15:14:17 -0500

    extent reports are completed for appsTester.java

--- src/test/java/com/online5/tests/apps/AppTests.java
+++ src/test/java/com/online5/tests/apps/AppTests.java
@@ -79,33 +79,44 @@ public class AppTests extends TestBase {
 	public void SPA_1073_searchAnAppNameNegative() {
 		extentLogger=report.createTest("APPS - Search an application name - Positive");
 		Homepage homePage = new Homepage();
+		extentLogger.info("Go to the website");
 		driver.get(ConfigurationReader.getProperty("url"));
 		BrowserUtils.waitForPageToLoad(2);
 
 		// assert that homepage titles matches
+		extentLogger.info("assert that homepage titles matches");
 		assertEquals(driver.getTitle(), "Free Calorie Counter, Diet & Exercise Journal | MyFitnessPal.com");
-
+		extentLogger.pass("Verified that homepage titles matches");
+		
 		// click on Apps menu
+		extentLogger.info("click on Apps menu");
 		homePage.AppsLink.click();
 
 		AppsPage appsPage = new AppsPage();
 		// assert if we see App Marketplace on the page
+		extentLogger.info("assert if we see App Marketplace on the page");
 		assertEquals(appsPage.appMarketPlaceTitle.getText(), "App Marketplace",
 				"H2 text does not match on search page");
-
+		extentLogger.pass("Verified App Marketplace on the page");
+		
 		// choose a wrong search item name
+		extentLogger.info("choose a wrong search item name");
 		String appName = "aaazzz";
 
 		// search on the searchbox on appspage
+		extentLogger.info("search on the searchbox on appspage");
 		appsPage.appSearch.sendKeys(appName + Keys.ENTER);
 		AppsSearchPage appsSearchPage = new AppsSearchPage();
 
 		// Assert if the page header is "Search Result"
+		extentLogger.info("Assert if the page header is \"Search Result\"");
 		assertTrue(appsSearchPage.searchResultHeader.getText().contains("Search Results"));
-
+		extentLogger.pass("Verified the page header is \\\"Search Result\\\"");
+		
 		// Assert if "0" items are found
+		extentLogger.info("Assert if \"0\" items are found");
 		assertTrue(appsSearchPage.searchResultCount() == 0);
-
+		extentLogger.pass("Verified \"0\" items are found");
 
 	}
 
@@ -113,41 +124,59 @@ public class AppTests extends TestBase {
 	public void SPA_1086_selectAnApp() {
 		extentLogger= report.createTest("APPS - Select an App - Positive");
 		Homepage homePage = new Homepage();
+		extentLogger.info("Go to the website");
 		driver.get(ConfigurationReader.getProperty("url"));
 		BrowserUtils.waitForPageToLoad(2);
 
 		// assert that homepage titles matches
+		extentLogger.info("assert that homepage titles matches");
 		assertEquals(driver.getTitle(), "Free Calorie Counter, Diet & Exercise Journal | MyFitnessPal.com");
-
+		extentLogger.pass("Verified homepage titles matches");
+		
 		// click on Apps menu
+		extentLogger.info("click on Apps menu");
 		homePage.AppsLink.click();
 
 		AppsPage appsPage = new AppsPage();
 		// assert if we see App Marketplace on the page
+		extentLogger.info("assert if we see App Marketplace on the page");
 		assertEquals(appsPage.appMarketPlaceTitle.getText(), "App Marketplace",
 				"H2 text does not match on search page");
-
+		extentLogger.pass("Verified App Marketplace on the page");
+			
+		
 		// Select one the a random Featured app under "Featured Apps"
 		WebElement randomApp = appsPage.getRandomFeaturedApp();
 		String randomAppName= randomApp.getText();
 		// Click on the selected app link and Assert if we see "App Gallery" and "<app
 		// name>"
+		extentLogger.info("Click on the selected app link ");
 		randomApp.click();
 
 		AppsSingleAppPage singleApp = new AppsSingleAppPage();
+		extentLogger.info("Assert if we see App Gallery in breadcrumbs");
 		assertTrue(singleApp.breadcrumbs.getText().contains("App Gallery"));
+		extentLogger.pass("Verified App Gallery in breadcrumbs");
+		
+		extentLogger.info("Assert if we see AppName in breadcrumbs");
 		assertTrue(singleApp.breadcrumbs.getText().contains(randomAppName));
-
+		extentLogger.pass("Verified AppName in breadcrumbs");
+		
 		// assert if the randomApp name matches on the page
 		String ActualAppName=  singleApp.appName.getText();
+		extentLogger.info("assert if the randomApp name matches on the page");
 		assertEquals(ActualAppName,randomAppName);
+		extentLogger.pass("Verified the randomApp name matches on the page");
 		
 		//Check if CONNECT button is displayed
+		extentLogger.info("Check if CONNECT button is displayed");
 		assertTrue(singleApp.connectButton.isDisplayed());
+		extentLogger.pass("Verified CONNECT button is displayed");
 		
 		// Assert if GET button is displayed
+		extentLogger.info("Assert if GET button is displayed");
 		assertTrue(singleApp.getButton.isDisplayed());	
-
+		extentLogger.pass("Verified  GET button is displayed");
 	}
 
 }
