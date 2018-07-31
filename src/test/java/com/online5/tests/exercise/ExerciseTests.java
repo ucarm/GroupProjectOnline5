f56408989764cd81e94a5bd3be39022c2b9fe459
Feruza <feruza_yusup@yahoo.com>
2018-07-31 17:35:28 -0500

    Extent report added

--- src/test/java/com/online5/tests/exercise/ExerciseTests.java
+++ src/test/java/com/online5/tests/exercise/ExerciseTests.java
@@ -18,6 +18,7 @@ public class ExerciseTests extends TestBase {
 
 	@BeforeMethod
 	public void navigateToExercisePage() {
+		extentLogger.info("Navigate to the website");
 		driver.get(ConfigurationReader.getProperty("url"));
 		BrowserUtils.waitForPageToLoad(2);
 		exercisePage = new ExerciseMainPage();
@@ -26,38 +27,54 @@ public class ExerciseTests extends TestBase {
 	
 	@Test
 	public void ExercisePageVerify() {
+		extentLogger=report.createTest("EXERCISE Page Verification");
 		String expectedTitle = "Calories Burned From Exercise | MyFitnessPal.com";
 		String actualTitle = driver.getTitle();
 		Assert.assertEquals(actualTitle, expectedTitle);
+		extentLogger.info("Assert if actual Title matching with Expected Title");
+		extentLogger.pass("Verified Expected Page Title is matching with Actual Title");
 	
 	}
 	
 	@Test
 	public void ExerciseSearchBoxTest() {
+		extentLogger=report.createTest("SEARCH Box Verification in Exercise Page");
 		exercisePage.exerciseSearchInput.clear();
 		exercisePage.exerciseSearchInput.sendKeys("tennis");
 		exercisePage.exerciseSearchButton.click();
 		assertTrue(exercisePage.dataInTheBox.getText().contains("tennis"));
+		extentLogger.info("Testing Search Box Functionality in the Exercise Page with valid input");
+		extentLogger.pass("Verified when user type <<tennis>> , it is displayed in matching exercises box");
 
 	}
 	
 	@Test
 	public void ExerciseSearchBoxNegative() {
+		extentLogger=report.createTest("SEARCH Box in Exercise Page Verification with Invalid Input");
 		exercisePage.exerciseSearchInput.clear();
 		exercisePage.exerciseSearchInput.sendKeys("abcdefg");
 		exercisePage.exerciseSearchButton.click();
 		assertTrue(exercisePage.errorMsgInTheBox.isDisplayed());
+		extentLogger.info("Verifying the Error Message in the Matching Exercise Box ");
+		extentLogger.pass("Verified when user type <<abcdefg>> Error message is displayed in the Matching Exercise Box");
 
 	}
 
 	@Test
-	public void ExerciseCaloriesBurnBox() throws InterruptedException {
+	public void ExerciseCaloriesBurnBox() {
+		extentLogger=report.createTest("Matching Exercise Box and Calories Burned Box Verification");
 		exercisePage.exerciseSearchInput.clear();
 		exercisePage.exerciseSearchInput.sendKeys("tennis");
+		extentLogger.info("Verifying SEARCH box functionality");
 		exercisePage.exerciseSearchButton.click();
+		extentLogger.info("Clicking on Serach Button");
 		exercisePage.FirstInputInTheBox.click();
-		Thread.sleep(5000);	
+		extentLogger.info("Clicking on <<Tennis, double>> link");
+		extentLogger.info("Verifying Matching Exercise data with Calories Burned Box data");
 		Assert.assertTrue(exercisePage.caloriesBurnedBox.getText().contains("Tennis, double"));
+		extentLogger.pass("Verified when user type <<tennis>> , it is displayed in matching exercises box");
+		extentLogger.pass("Verified the user click on <<Tennis, double>>,  <<Tennis, double>> should also displayed in Calories Burned Box");
+
 	}
 
 	@AfterMethod
