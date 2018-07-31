6d88808e6bbe748d6e053cadbf6cb600674fb4d0
akolbaev <azamat@na.edu>
2018-07-30 21:54:36 -0500

    Food Search TestCasesUpdated

--- src/test/java/com/online5/tests/food/FoodTests.java
+++ src/test/java/com/online5/tests/food/FoodTests.java
@@ -24,24 +24,35 @@ public class FoodTests extends TestBase {
 		foodPage.FoodNavigation.click();
 		
 	}
-	
+
 	@Test
-	public void foodPageDisplayTest()  {
+	public void foodSearchTestPositive() {
 		//Navigate to Food Page
 		//Assert that FoodMage title matches
 		String actualTitle = driver.getTitle();
 		String expectedTitle ="Calorie Chart, Nutrition Facts, Calories in Food | MyFitnessPal.com";
 		assertEquals(actualTitle, expectedTitle);
-		
-	}
-	
-	@Test
-	public void foodSearchTest() {
+		//Navigate to Food Search Page and get Result after searching for an apple
 		foodPage = new FoodMainPage();
-		foodPage.FoodSearchInput.sendKeys("Apple");
+		foodPage.FoodSearchInput.sendKeys(ConfigurationReader.getProperty("foodToSearch"));
 		foodPage.FoodSearchButton.click();
 		String firstFoodResult = foodPage.FoodSearchResults.get(0).getText();
-		System.out.println(firstFoodResult);
+		System.out.println("First food from search result: "+firstFoodResult);
 		
+		//Check if all results contain the word searched (in this case "Apple")
+		System.out.println("Food result list contains searched food: "+foodPage.checkIfResultsContainFood());
+		assertEquals( foodPage.checkIfResultsContainFood(), true);
+		}
+	
+	@Test
+	public void foodSearchTestNegative() {
+		foodPage = new FoodMainPage();
+		foodPage.FoodSearchInput.sendKeys("fdsa");
+		foodPage.FoodSearchButton.click();
+		String actualSearchResult =foodPage.invalidFoodInput.getText();
+		assertEquals(actualSearchResult, "No foods found.");
+		System.out.println("Invalid food name entry search result: "+actualSearchResult);
 	}
+	
+	
 }
